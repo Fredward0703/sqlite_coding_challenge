e6fa629d021cd618519e540ae014be47650a9f82
Federico Montejo <fmontejo@usf.edu>
2025-11-16 22:17:49 -0500

    Finished the first task

---
+++ INSIGHTS.md
@@ -0,0 +1,2 @@
+TASK 1:
+- Shows which of the top 5 customers had ordered the most 
--- challenge.sql
+++ challenge.sql
@@ -1,2 +1,8 @@
-SELECT * 
-FROM departments;
+-- Task 1
+-- Used the count feature to get the amount of times a customer had ordered, 
+--also used the JOIN function to join the customer and order tables to complete the task
+SELECT c.first_name, c.last_name, COUNT(o.customer_id) AS total_spent
+FROM customers c JOIN orders o on c.id = o.customer_id
+GROUP BY c.id, c.first_name,c.last_name
+ORDER BY total_spent DESC
+LIMIT 5;
