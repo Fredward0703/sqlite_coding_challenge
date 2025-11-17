6f6705f3bb8001448d0e772cb240e3ea932a005f
Federico Montejo <fmontejo@usf.edu>
2025-11-16 22:56:58 -0500

    Completed task 2

--- INSIGHTS.md
+++ INSIGHTS.md
@@ -1,2 +1,5 @@
 TASK 1:
 - Shows which of the top 5 customers had ordered the most 
+TASK 2: 
+- Gives us which category of products gives us the most revenue
+- Shows that the biggest earner is electronics by a mile with it earning twice as much as the second leader, furniture.
--- challenge.sql
+++ challenge.sql
@@ -1,4 +1,4 @@
--- Task 1
+-- Task 1:
 -- Used the count feature to get the amount of times a customer had ordered, 
 --also used the JOIN function to join the customer and order tables to complete the task
 SELECT c.first_name, c.last_name, COUNT(o.customer_id) AS total_spent
@@ -6,3 +6,10 @@ FROM customers c JOIN orders o on c.id = o.customer_id
 GROUP BY c.id, c.first_name,c.last_name
 ORDER BY total_spent DESC
 LIMIT 5;
+
+--Task 2:
+-- same as task 1, joined the products and order items table to give the revenue using the unit price and quantity
+SELECT p.category, SUM(oi.quantity * oi.unit_price) AS revenue
+FROM order_items oi JOIN products p on oi.product_id = p.id
+GROUP BY p.category
+ORDER BY revenue DESC
